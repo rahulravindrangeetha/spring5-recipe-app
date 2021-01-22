c687dd83bb226b9d9d5d7f9258dcf0f41b4548ea
rahulie <rahulrg150889@gmail.com>
2021-01-22 07:17:05 +0530

    initial commit

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class Spring5RecipeAppApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
